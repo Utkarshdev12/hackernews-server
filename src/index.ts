af008647e8a4278864db67840ada179158a1daec
Utkarsh <utkarshdev12@gmail.com>
2025-04-04 14:17:54 +0530

    refactor: update package.json and package-lock.json, add Dockerfile, and enhance tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { allroutes } from "./routes/routes.js";
 import { serve } from "@hono/node-server";
 
+
 serve(
   {
     fetch: allroutes.fetch,
     port: 3000,
   },
   (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`)
+    console.log(`✅ Server is running on http://localhost:${info.port}`);
   }
-)
+);
+
