1b104e3fe99fa0122e9110680bae32bc4dabbf85
Utkarsh <utkarshdev12@gmail.com>
2025-04-04 11:21:15 +0530

    initialize project structure with basic server setup and Prisma integration

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { allroutes } from "./routes/routes.js";
+import { serve } from "@hono/node-server";
+
+serve(
+  {
+    fetch: allroutes.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  }
+)
