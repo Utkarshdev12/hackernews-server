00deb62ae60c9b671f9f5ae9d60ea2efed9dc591
Utkarsh <utkarshdev12@gmail.com>
2025-04-04 14:54:47 +0530

    chore: no code changes made in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { allroutes } from "./routes/routes.js";
 import { serve } from "@hono/node-server";
 
+const port = Number(process.env.PORT) || 3000;
 
 serve(
   {
     fetch: allroutes.fetch,
-    port: 3000,
+    port,
   },
   (info) => {
-    console.log(`✅ Server is running on http://localhost:${info.port}`);
+    console.log(`✅ Server is running on http://0.0.0.0:${info.port}`);
   }
 );
-
